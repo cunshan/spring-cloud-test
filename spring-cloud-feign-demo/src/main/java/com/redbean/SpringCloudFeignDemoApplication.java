c3e6ca7d1c1ba0546a27ff255783877015029239
王存山 <18661877367@163.com>
2017-05-04 10:11:33 +0800

    spring cloud  消费者 feign demo

---
+++ spring-cloud-feign-demo/src/main/java/com/redbean/SpringCloudFeignDemoApplication.java
@@ -0,0 +1,12 @@
+package com.redbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFeignDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudFeignDemoApplication.class, args);
+	}
+}
