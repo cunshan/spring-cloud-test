b35a2035f0c613cfc181e18526062c9b04633806
王存山 <18661877367@163.com>
2017-05-16 10:55:41 +0800

    config client demo

---
+++ spring-cloud-config-client-demo/src/main/java/com/redbean/SpringCloudConfigClientDemoApplication.java
@@ -0,0 +1,12 @@
+package com.redbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientDemoApplication.class, args);
+	}
+}
