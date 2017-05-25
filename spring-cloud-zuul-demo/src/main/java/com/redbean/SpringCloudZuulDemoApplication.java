361a84a0ce67f723c8af496d9038bdea75c113ad
王存山 <18661877367@163.com>
2017-05-25 13:34:10 +0800

    spring cloud zuul demo

---
+++ spring-cloud-zuul-demo/src/main/java/com/redbean/SpringCloudZuulDemoApplication.java
@@ -0,0 +1,15 @@
+package com.redbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringCloudApplication
+@EnableZuulProxy
+public class SpringCloudZuulDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudZuulDemoApplication.class, args);
+  }
+}
